37fb3e735489b7e18568b6a92740cc62c5b3a40d
Yang <YJJProyectos@users.noreply.github.com>
2017-12-07 00:24:10 -0300

    Mas ejemplos, FK, ALTER TABLE

--- SQL ejemplos.sql
+++ SQL ejemplos.sql
@@ -160,7 +160,7 @@ INSERT INTO books (author, title, words)
 INSERT INTO books (author, title, words)
     VALUES ("J.R.R. Tolkien", "Return of the King", 134462);
  
-SELECT author,SUM(words) as total_words FROM books GROUP BY author HAVING total_words >= 1000000 
+SELECT author,SUM(words) as total_words FROM books GROUP BY author HAVING total_words >= 1000000;
 SELECT author,AVG(words) as avg_words FROM books GROUP BY author HAVING avg_words >= 150000;
 
 SELECT author,
@@ -258,3 +258,108 @@ SELECT persons.name,hobbies.name FROM persons
     ON persons.id = hobbies.person_id
     WHERE persons.name = "Jia";
 
+	
+CREATE TABLE clothes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    type TEXT,
+    design TEXT);
+    
+INSERT INTO clothes (type, design)
+    VALUES ("dress", "pink polka dots");
+INSERT INTO clothes (type, design)
+    VALUES ("pants", "rainbow tie-dye");
+INSERT INTO clothes (type, design)
+    VALUES ("blazer", "black sequin");
+ALTER TABLE clothes ADD price INTEGER; 
+SELECT * FROM clothes;
+UPDATE clothes SET price=10 WHERE id = 1;
+SELECT * FROM clothes;
+UPDATE clothes 
+SET price=
+    CASE 
+        WHEN id = 1
+            THEN 10
+        WHEN id = 2
+            THEN 20
+        WHEN id = 3
+            THEN 30
+    END ;
+SELECT * FROM clothes;
+
+
+
+CREATE TABLE animales (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    clave INTEGER
+);
+
+CREATE TABLE foranea(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+/*    algo FOREING KEY (id), */
+    name TEXT,
+  artist INTEGER,
+  FOREIGN KEY(artist) REFERENCES animales(id)
+);
+CREATE TABLE cosas( 
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    clave INTEGER
+);
+CREATE TABLE osos(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    pies TEXT
+);
+ALTER TABLE osos ADD cosas INTEGER;
+INSERT INTO osos VALUES(1,"lala", 3);
+SELECT * FROM osos; 
+
+
+
+CREATE TABLE students (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    email TEXT,
+    teacher_id INTEGER);
+    
+CREATE TABLE teachers (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    email TEXT);
+
+INSERT INTO teachers (name, email)
+    VALUES ("Socrates", "soc@rat.es");
+INSERT INTO teachers (name, email)
+    VALUES ("Maria Montessori", "maria@montessori.com");
+    
+INSERT INTO students (name, email, teacher_id)
+    VALUES ("Antisthenes", "jr@winston.com", 1);
+INSERT INTO students (name, email, teacher_id)
+    VALUES ("Xenophon", "xenophon@greece.com", 1);
+INSERT INTO students (name, email, teacher_id)
+    VALUES ("Plato", "plato@greece.com", 1);
+INSERT INTO students (name, email, teacher_id)
+    VALUES ("Larry", "larry@page.com", 2);
+INSERT INTO students (name, email, teacher_id)
+    VALUES ("Sergey", "larry@page.com", 2);
+INSERT INTO students (name, email, teacher_id)
+    VALUES ("Ray", "ray@bradbury.com", NULL);
+
+SELECT * FROM students;
+SELECT * FROM teachers;
+
+
+/* Show students next to their teachers */
+SELECT students.name, teachers.name AS teacher_name
+    FROM students
+    JOIN teachers
+    ON students.teacher_id = teachers.id;
+  
+/* Show students next to their teachers,
+   even if they don't have a teacher assigned
+*/ 
+SELECT students.name, teachers.name AS teacher_name
+    FROM students
+    LEFT OUTER JOIN teachers
+    ON students.teacher_id = teachers.id;
+
+SELECT email, COUNT(teacher_id) as numero FROM students
+GROUP BY email ORDER BY numero DESC,email ASC;
